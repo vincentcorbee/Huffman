40089544b7df27512a3f842c18586d92ce78b55d
Vincent Corbee <vincentcorbee@Vincents-MacBook-Pro.local>
2022-12-27 16:45:47 +0100

    feat(index) updated decoding

--- src/index.js
+++ src/index.js
@@ -6,21 +6,32 @@ const text = `You should call this method whenever you're ready to update your a
 
 The callback method is passed a single argument, a DOMHighResTimeStamp, which indicates the current time (based on the number of milliseconds since time origin). When multiple callbacks queued by requestAnimationFrame() begin to fire in a single frame, each receives the same timestamp even though time has passed during the computation of every previous callback's workload (in the code example below we only animate the frame when the timestamp changes, i.e. on the first callback). This timestamp is a decimal number, in milliseconds, but with a minimal precision of 1ms (1000 µs).`
 
+const text2 = 'abracadabra'
+
 class BinaryReader {
   constructor(arrayBuffer) {
     this.view = new DataView(arrayBuffer)
     this.pos = 0
-    this.bit_pos = -1
+    this.bitPos = -1
     this.chunk = null
-    this.bit_count = 0
-    // this.bits_read = 0
+    this.bitCount = 0
+    // this.bitsRead = 0
+  }
+
+  get buffer () {
+    return this.view.buffer
+  }
+
+  getBytePosition() {
+    return this.pos
   }
 
   seek(pos) {
-    const old_pos = this.pos
+    const oldPos = this.pos
+
     this.pos = pos
 
-    return old_pos
+    return oldPos
   }
 
   peak(index = this.pos + 1) {
@@ -34,16 +45,16 @@ class BinaryReader {
   peakBit() {
     const chunk = this.chunk
     const pos = this.pos
-    const bit_pos = this.bit_pos
-    const bit_count = this.bit_count
-    // const bits_read = this.bits_read
+    const bitPos = this.bitPos
+    const bitCount = this.bitCount
+    // const bitsRead = this.bitsRead
     const bit = this.getBit()
 
-    this.bit_pos = bit_pos
+    this.bitPos = bitPos
     this.chunk = chunk
     this.pos = pos
-    this.bit_count = bit_count
-    // this.bits_read = bits_read
+    this.bitCount = bitCount
+    // this.bitsRead = bitsRead
 
     return bit
   }
@@ -52,9 +63,9 @@ class BinaryReader {
     if (this.chunk === null) {
       return null
     } else {
-      const bit_count = getBitCount(this.chunk)
+      const bitCount = getBitCount(this.chunk)
 
-      return 8 - bit_count
+      return 8 - bitCount
     }
   }
 
@@ -63,30 +74,28 @@ class BinaryReader {
   }
 
   getBit() {
-    if (this.bit_pos === -1) {
+    if (this.bitPos === -1) {
       this.chunk = this.getData()
 
-      this.bit_pos = this.getBitPos()
-      this.bit_count = getBitCount(this.chunk)
+      this.bitPos = this.getBitPos()
+      this.bitCount = getBitCount(this.chunk)
     }
 
     if (this.chunk === null) {
       return null
     }
 
-    const bit_count = this.bit_count
-    const bit = this.bit_pos >= bit_count ? 0 : (this.chunk >> this.bit_pos) & 1
+    const bitCount = this.bitCount
+    const bit = this.bitPos >= bitCount ? 0 : (this.chunk >> this.bitPos) & 1
 
-    this.bit_pos--
-
-    //  this.bits_read ++
+    this.bitPos--
 
     return bit
   }
 
   getData(type = 'Uint8') {
     if (this.view.byteLength > this.pos) {
-      this.bit_pos = -1
+      this.bitPos = -1
 
       return this.view[`get${type}`](this.pos++)
     }
@@ -112,13 +121,60 @@ class BinaryReader {
   }
 }
 
+class BinaryWriter extends BinaryReader {
+  constructor(length) {
+    super(new ArrayBuffer(length))
+
+    this.length = length
+  }
+
+  setData(data, type = 'Uint8') {
+    let advance = 0
+
+    switch(type) {
+      case 'Uint16':
+        advance = 2
+        break;
+      case 'Uint32':
+        advance = 4
+        break;
+      default:
+        advance = 1
+    }
+
+    if (this.view.byteLength > this.pos) {
+      this.bitPos = -1
+
+      this.view[`set${type}`](this.pos, data)
+
+      this.pos += advance
+
+      return this
+    }
+
+    return this
+  }
+
+  setUint32(data) {
+    return this.setData(data, 'Uint32')
+  }
+
+  setUint16(data) {
+    return this.setData(data, 'Uint16')
+  }
+
+  setUint8(data) {
+    return this.setData(data)
+  }
+}
+
+
+
 const getIndex = (items, count) => {
   let i = items.length - 1
 
   while (i > 0) {
-    if (count <= items[i][1]) {
-      return i
-    }
+    if (count <= items[i][1]) return i
 
     i--
   }
@@ -126,11 +182,11 @@ const getIndex = (items, count) => {
   return 0
 }
 
-const getEncoding = (items, char) => {
-  const [start_node] = items
+const getEncoding = (huffmanTree, char) => {
+  const [startNode] = huffmanTree
 
   const traverse = (node, char, encoding = []) => {
-    const [str, count, nodes] = node
+    const [str, _count, nodes] = node
 
     if (char === str) {
       return encoding
@@ -145,54 +201,204 @@ const getEncoding = (items, char) => {
     }
   }
 
-  return traverse(start_node, char)
+  return traverse(startNode, char)
 }
 
 const createHuffmanTree = items => {
-  let i = 0
-
   while (items.length > 1) {
     const a = items.pop()
     const b = items.pop()
 
     const [char_a, count_a] = a
     const [char_b, count_b] = b
-    const new_count = count_a + count_b
-    const new_index = getIndex(items, new_count)
-    const internal_node = [`${char_a}${char_b}`, new_count, [a, b]]
 
-    items.splice(new_index, 0, internal_node)
+    const newCount = count_a + count_b
 
-    i += 2
+    const newIndex = getIndex(items, newCount)
+    const internal_node = [`${char_a}${char_b}`, newCount, [a, b]]
+
+    items.splice(newIndex, 0, internal_node)
   }
 
   return items
 }
 
 const getBitCount = number => {
-  let bit_count = 0
+  let bitCount = 0
 
   while (number) {
     number >>= 1
-    bit_count++
+    bitCount++
   }
 
-  return bit_count
+  return bitCount
+}
+
+const mapCodeToInteger = (code) =>
+  code.reduce((acc, bit) => (acc << 1) | bit, 0)
+
+const getCharacterCode = (char) => char.charCodeAt(0)
+
+const getByteCount = bitCount => {
+  const remainder = bitCount % 8
+
+  return (bitCount + (remainder ? 8 - remainder : 0)) / 8
+}
+
+const createFrequentieTable = (input) => {
+  const frequentieTable = {}
+
+  for (let i = 0, l = input.length; i < l; i++) {
+    const char = input.charAt(i)
+
+    if (frequentieTable[char] !== undefined) {
+      frequentieTable[char]++
+    } else {
+      frequentieTable[char] = 1
+    }
+  }
+
+  return frequentieTable
+}
+
+const toArrayBuffer = (data, encodingTable) => {
+  /* Two bytes for the length of the encoding table, four bytes for the bit count of the encoded data  */
+  const LENGTH_HEADER_OFFSET = 6
+
+  let lengthEncodingTable = 0
+
+  const entries = Object.entries(encodingTable).map(([char, code]) => {
+    const { length } = code
+
+    /*
+      We add 2 bytes for the length and character and 1 or 2 for the code depending
+      on wether it's code length is bigger then 8 bits
+    */
+
+    lengthEncodingTable += 2 + (length > 8 ? 2 : 1)
+
+    return [length, getCharacterCode(char), mapCodeToInteger(code)]
+  })
+
+  const bitCount = data.length
+  const byteCount = getByteCount(bitCount)
+  const padCountEncodedData = bitCount % 8
+
+  const binaryWriter = new BinaryWriter(byteCount + LENGTH_HEADER_OFFSET + lengthEncodingTable)
+
+  const buffer = new ArrayBuffer(byteCount + LENGTH_HEADER_OFFSET + lengthEncodingTable)
+  const dataView = new DataView(buffer)
+
+  let offsetAdjustment = 0
+
+  binaryWriter.setUint16(lengthEncodingTable)
+
+  dataView.setUint16(0, lengthEncodingTable)
+
+  entries.forEach(([length, charCode, code], i) => {
+    const offset = i * 3 + 2
+
+    binaryWriter.setUint8(length)
+    binaryWriter.setUint8(charCode)
+
+    dataView.setUint8(offset + offsetAdjustment, length)
+    dataView.setUint8(offset + 1 + offsetAdjustment, charCode)
+
+    if (length > 8) {
+      binaryWriter.setUint16(code)
+      dataView.setUint16(offset + 2 + offsetAdjustment, code)
+
+      offsetAdjustment++
+    } else {
+      binaryWriter.setUint8(code)
+      dataView.setUint8(offset + 2 + offsetAdjustment, code)
+    }
+  })
+
+  let chunk = 0
+  let offsetChunk = LENGTH_HEADER_OFFSET + lengthEncodingTable
+  let index = 0
+
+  // Store encoded length as uint32
+
+  binaryWriter.setUint32(bitCount)
+
+  dataView.setUint32(lengthEncodingTable + 2, bitCount)
+
+  while (index < bitCount) {
+    if (index % 8 === 0 && index !== 0) {
+      binaryWriter.setUint8(chunk)
+
+      dataView.setUint8(offsetChunk, chunk)
+
+      offsetChunk++
+
+      chunk = 0
+    }
+
+    const bit = data[index]
+
+    chunk = (chunk << 1) | bit
+
+    index++
+  }
+
+  if (chunk) {
+    for (let pad = 0; pad <= padCountEncodedData; pad++) {
+      chunk = chunk << 1
+    }
+
+    // const bitCountChunk = getBitCount(chunk)
+
+    // Pad last chunk if not 8 bits
+    // let padding = 8 - bitCountChunk - (bitCount - (8 * (offsetChunk - LENGTH_HEADER_OFFSET - lengthEncodingTable) + bitCountChunk))
+
+    // while (padding > 0) {
+    //   chunk = chunk << 1
+
+    //   padding--
+    // }
+
+    binaryWriter.setUint8(chunk)
+
+    dataView.setUint8(offsetChunk, chunk)
+  }
+
+  return binaryWriter.buffer
+  // return buffer
+}
+
+const encode = (input) => {
+  const frequentieTable = createFrequentieTable(input)
+  const huffmanTree = createHuffmanTree(Object.entries(frequentieTable).sort((a, b) => b[1] - a[1]))
+  const encodingTable = Object.keys(frequentieTable).reduce((acc, char) => {
+    acc[char] = getEncoding(huffmanTree, char)
+
+    return acc
+  }, {})
+
+  const encodedData  = input
+    .split('')
+    .flatMap(char => encodingTable[char])
+
+  return toArrayBuffer(encodedData , encodingTable)
 }
 
 const decode = buffer => {
-  const br = new BinaryReader(buffer)
-  const decoded_data = []
+  const binaryReader = new BinaryReader(buffer)
+  const decodedData = []
 
-  const header_length = br.getUint16()
+  const headerLength = binaryReader.getUint16()
   const header = ['', 0, []]
+
   let i = 0
 
-  while (i < header_length) {
-    let length = br.getUint8()
-    let index = length - 1
-    const char = String.fromCharCode(br.getUint8())
-    const code = length > 8 ? br.getUint16() : br.getUint8()
+  while (i < headerLength) {
+    let lengthCode = binaryReader.getUint8()
+    let index = lengthCode - 1
+
+    const char = String.fromCharCode(binaryReader.getUint8())
+    const code = lengthCode > 8 ? binaryReader.getUint16() : binaryReader.getUint8()
 
     let entry = header
 
@@ -224,155 +430,45 @@ const decode = buffer => {
       index--
     }
 
-    i += 1 + 1 + (length > 8 ? 2 : 1)
+    i += 2 + (lengthCode > 8 ? 2 : 1)
   }
 
-  // console.log(header)
+  console.log(binaryReader.getBytePosition())
 
-  br.seek(header_length + 2)
+  // binaryReader.seek(headerLength + 3)
 
-  // First 4 bytes are length of the encoded data
-  const length = br.getUint32()
-  let bits_read = 0
+  /* These 4 bytes are the length of the encoded data */
 
-  const traverse = (node, br) => {
-    if (bits_read > length) return ''
+  const length = binaryReader.getUint32()
+
+  console.log(length, headerLength)
+
+  let bitsRead = 0
+
+  const traverse = (node, binaryReader) => {
+    if (bitsRead > length) return ''
 
     const nodes = node[2]
 
     if (!nodes) return node[0]
 
-    const bit = br.getBit()
+    const bit = binaryReader.getBit()
 
-    bits_read++
+    bitsRead++
 
     if (bit !== null) {
-      if (nodes) {
-        return traverse(nodes[bit], br)
-      } else {
-        return node[0]
-      }
+      if (nodes) return traverse(nodes[bit], binaryReader)
+      else return node[0]
     } else {
       return ''
     }
   }
 
-  while (bits_read < length && br.peakBit() !== null) {
-    decoded_data.push(traverse(header, br))
+  while (bitsRead < length && binaryReader.peakBit() !== null) {
+    decodedData.push(traverse(header, binaryReader))
   }
 
-  return decoded_data
-}
-
-const getByteCount = bit_count => {
-  const remainder = bit_count % 8
-  const byte_count = (bit_count + (remainder ? 8 - remainder : 0)) / 8
-
-  return byte_count
-}
-
-const toArrayBuffer = (data, enc_table) => {
-  let c2 = 0
-
-  const entries = Object.entries(enc_table).map(([char, code]) => {
-    const l = code.length
-
-    c2 += 1 + 1 + (l > 8 ? 2 : 1)
-
-    return [l, char.charCodeAt(0), code.reduce((acc, bit) => (acc << 1) | bit, 0)]
-  })
-
-  const bit_count = data.length
-  const byte_count = getByteCount(bit_count)
-  const buffer = new ArrayBuffer(byte_count + 6 + c2)
-  const view = new DataView(buffer)
-  let offset = 0
-
-  view.setUint16(0, c2)
-
-  entries.forEach(([length, char_code, code], i) => {
-    view.setUint8(i * 3 + 2 + offset, length)
-    view.setUint8(i * 3 + 2 + 1 + offset, char_code)
-
-    if (length > 8) {
-      view.setUint16(i * 3 + 2 + 2 + offset, code)
-
-      offset++
-    } else {
-      view.setUint8(i * 3 + 2 + 2 + offset, code)
-    }
-  })
-
-  let chunk = 0
-  let count = 6 + c2
-  let i = 0
-
-  // Store encoded length as uint32
-  view.setUint32(c2 + 2, bit_count)
-
-  while (data.length) {
-    if (i % 8 === 0 && i !== 0) {
-      view.setUint8(count, chunk)
-      count++
-      chunk = 0
-    }
-
-    const bit = data.shift()
-
-    chunk = (chunk << 1) | bit
-
-    i++
-  }
-
-  if (chunk) {
-    const c = getBitCount(chunk)
-
-    // Pad last chunk if not 8 bits
-    let padding = 8 - c - (bit_count - (8 * (count - 6 - c2) + c))
-
-    while (padding > 0) {
-      chunk = chunk << 1
-
-      padding--
-    }
-
-    view.setUint8(count, chunk)
-  }
-
-  return buffer
-}
-
-const createFrequentieTable = (input) => {
-  const freq_table = {}
-
-  for (let i = 0, l = input.length; i < l; i++) {
-    const char = input.charAt(i)
-
-    if (freq_table[char] !== undefined) {
-      freq_table[char]++
-    } else {
-      freq_table[char] = 1
-    }
-  }
-
-  return freq_table
-}
-
-const encode = (input) => {
-  const freq_table = createFrequentieTable(input)
-  const tree = createHuffmanTree(Object.entries(freq_table).sort((a, b) => b[1] - a[1]))
-  const enc_table = Object.keys(freq_table).reduce((acc, char) => {
-    acc[char] = getEncoding(tree, char)
-
-    return acc
-  }, {})
-
-  const encoded_data = input
-    .split('')
-    .map(char => enc_table[char])
-    .flat()
-
-  return toArrayBuffer([...encoded_data], enc_table)
+  return decodedData
 }
 
 const encodedData = encode(text)
